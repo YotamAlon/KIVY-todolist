a4ef9dc87632c805233d02138d8dc4a41087b7cc
Yotam Alon <yotamalon@gmail.com>
2015-10-17 06:00:18 -0700

    .py file nearly done. .kv file remains

--- todolist.py
+++ todolist.py
@@ -1,5 +1,5 @@
 import kivy
-kivy.require('1.9.0')
+kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -10,6 +10,11 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 
 class Line(Label):
+	def __init__(self):
+		super(Line, self).__init__()
+		rmbtn = Button(text='Remove')
+		rmbtn.bind(on_press=self.remove)
+		self.add_widget(rmbtn)
 
 	def remove(self):
 		self.root.todo.remove(self.text)
@@ -20,20 +25,22 @@ class Menu(Widget):
 	
 	def __init__(self):
 		super(Menu, self).__init__()
+		self.load()
 		adder = Button(text='Add')
 		adder.bind(on_press=self.add)
+		self.add_widget(adder)
 
-	def load():
+	def load(self):
 		try:
 			f = open('todolist.dat', 'r')
 			for line in list(f):
 				self.todo.append(Line(text=line))
 				self.add_widget(Line(line))
-		except FileNotFoundError:
+		except:
 			f = open('todolist.dat', 'w')
 			
 		
-	def add():
+	def add(self):
 		new = Popup(text='New Entry:')
 		line = TextInput(multiline=False)
 		self.todo.append(Line(text=line))
@@ -45,7 +52,7 @@ class Menu(Widget):
 
 class TodoList(App):
 	def build(self):
-		return Menu().load()
+		return Menu()
 		
 if __name__ == '__main__':
 	TodoList().run()
